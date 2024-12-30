ac927caf545ee3a7e99a83fbebe67272ac8393bb
Alissa Troiano <alissatroianonyc@gmail.com>
2024-12-30 18:21:54 -0500

    Update testing app animations

--- src/createPost.tsx
+++ src/createPost.tsx
@@ -7,7 +7,7 @@ Devvit.configure({
 
 // Adds a new menu item to the subreddit allowing to create a new post
 Devvit.addMenuItem({
-  label: 'Create Slangman Post',
+  label: 'Test Slangman',
   location: 'subreddit',
   onPress: async (_event, context) => {
     const { reddit, ui } = context;
